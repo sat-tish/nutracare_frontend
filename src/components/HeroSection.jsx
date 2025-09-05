c7766d625ff537152470773d08fa4d66d992dde0
sat-tish <satish30yash@gmail.com>
2025-09-05 22:14:32 +0530

    second commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,51 @@
-//import heroBg from '../assets/images/background-images/b2.jpg'
+
+import { Link } from "react-router"
+import SelectCity from "./SelectCity"
+import DoctorCenter from "./DoctorCenter"
+import Category from "./Category"
 
 const HeroSection = () => {
   return (
-    <div className="w-full h-[80vh] bg-[url('./assets/images/background-images/b2.jpg')] bg-no-repeat bg-cover bg-center">
+    <div className="w-full md:h-[80vh] bg-gray-800 bg-[url('./assets/images/background-images/b2.jpg')] bg-no-repeat bg-cover bg-center bg-blend-soft-light">
+    <div className="w-[90%] h-full m-auto lg:grid grid-cols-2">
+         <div className="h-full flex justify-center items-center">
+        <div className=" sm:w-[70%] my-[50px] bg-white py-5 rounded shadow">
+            <h1 className="text-center mt-5 mb-7 px-5"> Find a Healthcare Professional / Center near you</h1>
+            <form action="" className="w-[90%] flex flex-col justify-center gap-y-4 m-auto">
+                <SelectCity />
+                <DoctorCenter />
+                <Category />
+                <button type="button" class="mb-5 w-[90%] m-auto text-white bg-[#0086C6] hover:bg-[#0473B9] focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base py-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+            </form>
+        </div>
+     </div>
+        <div className="m-auto text-white max-md:hidden">
+          <h1 className=" text-4xl font-semibold mb-[50px]">Nutracare Holistic way of Healing</h1>
+          <div className="grid grid-cols-3 m-2 text-sm">
+            <div className="flex flex-col justify-center ">
+                <Link className="text-gray-200 hover:text-white mb-1">Physiotherapist</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Nutritionist</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Osteopathy</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Traditional Chinese</Link>
+               
+            </div>
+            <div className="flex flex-col justify-center ">
+                <Link className="text-gray-200 hover:text-white mb-1">Homeopathy</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Ayurveda</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Yoga</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Accupressure</Link>
+            </div>
+            <div className="flex flex-col justify-center ">
+                <Link className="text-gray-200 hover:text-white mb-1">Chiropractor</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Naturopathy</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Ozone therapy</Link>
+                <Link className="text-gray-200 hover:text-white mb-1">Find More +</Link>
+            </div>
+          </div>
+     </div>
     
+     
+    </div>
     </div>
   )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,8 +7,14 @@ const Navigation = () => {
       <div className="flex items-center w-full h-full ps-5 sm:ps-8">
         <img className='h-10' src={logoImage} alt="" />
       </div>
-      <div className='flex justify-end items-center pe-5 sm:hidden'>nav button</div>
-      <div className='flex justify-end items-center pe-8 max-sm:hidden col-span-2 sm:space-x-6 md:space-x-10'>
+      <div className='flex justify-end items-center pe-5 sm:hidden'>
+       <button class="flex items-center justify-center border-[1px] bg-white text-gray-700 hover:text-gray-900 font-bold py-1 px-1 rounded">
+  <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+  </svg>
+</button>
+      </div>
+      <div className='flex justify-end items-center pe-8 max-sm:hidden col-span-2 sm:space-x-6 md:space-x-10 font-medium'>
        <Link to="/">Home</Link>
        <Link to="/categories">Categories</Link>
        <Link to="/blogs">Blogs</Link>
